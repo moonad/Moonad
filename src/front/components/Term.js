3647bfbb15127943a4e75c7397a2adfca3ca0e00
Victor Maia <srvictormaia@gmail.com>
2020-06-11 17:00:02 -0300

    Initial app renderer

--- src/front/components/Term.js
+++ src/front/components/Term.js
@@ -14,7 +14,7 @@ class Term extends Component {
     this.poid = null;
     this.defs = null;
     this.argm = [];
-    this.memo = {};
+    this.app = null;
   }
   async componentDidMount() {
     this.poid = await front.moonad.api.get_orig({name: this.props.name});
@@ -23,33 +23,57 @@ class Term extends Component {
   render() {
     const name = this.props.name;
     const defs = this.defs;
+
     if (!defs || !defs[name]) {
       return h("div", {}, "Loading...");
     } else {
-      var type = this.defs[name].type;
-      if (type.ctor === "All" && type.self === "self_App") {
-        return h("div", {}, "APP");
-      } else {
-        var head = h("div", {
+      var type = fm.synt.reduce(this.defs[name].type);
+
+      // If this is an application...
+      if ( type.ctor === "App"
+        && type.func.ctor === "Ref"
+        && type.func.name === "App.V0") {
+
+        // Type-checks, compiles and initializes application
+        if (!this.app) {
+          fm.synt.typesynth(name, defs, fm.lang.stringify);
+          var core_defs = {};
+          for (var def in defs) {
+            core_defs[def] = defs[def].core;
+          };
+          var js_code = fm.tojs.compile(name, core_defs, true);
+          console.log(js_code);
+          this.app = eval(js_code);
+          this.app = this.app[name];
+        }
+
+        // Renders app with its current state
+        var rendered = this.app.draw(this.app.init);
+
+        // Converts rendered object to an HTML element
+        var app_el = null;
+        switch (rendered._) {
+          case "App.V0.Render.text":
+            app_el = rendered.str;
+            break;
+        }
+
+        // Builds the body
+        return h("div", {
           style: {
-            "font-family": "IBMPlexMono-Light",
-            "user-select": "none",
-            "font-size": "16px",
-            "color": "rgb(41, 42, 44)",
-            "text-decoration": "underline",
-            "cursor": "pointer",
+            "width": "100% - 30px",
+            "min-height": "100%",
           },
           onClick: () => {
-            if (this.poid) {
-              front.set_route("/p/"+this.poid);
-            };
+            // When user clicks, sends an event to the app
+            var app_event = {_: "App.V0.Event.keypress", keycode: 0};
+            this.app.init = this.app.when(app_event)(this.app.init);
           },
-        }, this.props.name);
+        }, app_el);
 
-        var body = null;
-
-        // Arguments
-        var type = fm.synt.reduce(type);
+      // If this is just a function...
+      } else {
+        // Gathers arguments
         var argm_divs = [];
         var argm_type = [];
         var argm_eras = [];
@@ -83,7 +107,7 @@ class Term extends Component {
           }
         }
 
-        // Prints result
+        // Builds result
         if (argm_term.length === argm_type.length) {
           var term = fm.synt.Ref(name);
           for (let i = 0; i < argm_term.length; ++i) {
@@ -128,7 +152,25 @@ class Term extends Component {
           }
         };
 
-        body = h("div", {
+        // Term name
+        var head = h("div", {
+          style: {
+            "font-family": "IBMPlexMono-Light",
+            "user-select": "none",
+            "font-size": "16px",
+            "color": "rgb(41, 42, 44)",
+            "text-decoration": "underline",
+            "cursor": "pointer",
+          },
+          onClick: () => {
+            if (this.poid) {
+              front.set_route("/p/"+this.poid);
+            };
+          },
+        }, this.props.name);
+
+        // Term args and result
+        var body = h("div", {
           style: {
             "font-family": "IBMPlexMono-Light",
             "user-select": "none",
