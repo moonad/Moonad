d31514d664d78d067625b9c487634cf8cc82098c
Maisa <maisa.milena@gmail.com>
2020-07-29 19:31:30 -0300

    Update listener

--- app/src/back/utils/github_listener.js
+++ app/src/back/utils/github_listener.js
@@ -3,39 +3,51 @@ require('dotenv').config({path: path.join(__dirname, "..", "..", "..", ".env")})
 
 var express = require("express");
 var app = express();
-
-var { createHmac }  = require("crypto");
-
-var github_util = require("./github_util.js");
+const { exec } = require("child_process");
 
 const secret = process.env.GITHUB_WEBHOOK_SECRET || 'none';
+const port = process.env.LISTENER_PORT || 81;
+
+function git_pull() {
+  exec("git pull", (error, stdout, stderr) => {
+    console.log("exec git pull");
+    if (error) {
+      console.log(`error: ${error.message}`);
+      return;
+    }
+    if (stderr) {
+      console.log(`stderr: ${stderr}`);
+      return;
+    }
+    console.log(`stdout: ${stdout}`);
+  });
+}
 
 // Initialize
 async function startup() {
-  app.listen(81);
+  app.listen(port);
   console.log("Github listener open!");
 };
 
 startup();
 
 app.post("/github", async (req, res) => {
-  console.log("POST GITHUB");
-  if(verify_signature(req)) {
-    github_util.git_pull();
-    res.send("Hi, there. What's up? I'm up...dated");
-  } else {
-    console.log("not authorized");
-    res.send("You have no power here.");
-  }
+  console.log("Updating from Github...");
+  git_pull();git_pull
+  res.send("Hi, there. What's up? I'm up...dated");
 });
 
-// Auxiliar
-function verify_signature(req) {
-  const signature = req.headers['x-hub-signature']
-  return signature && (signature === sign(req.raw))
-}
+// // Auxiliar
+// function verify_signature(req) {
+//   // const signature = req.headers['x-hub-signature']
+//   console.log("req raw: ",req.raw);
+//   console.log(">>>> TYPE of:", typeof req.raw)
+//   console.log("signature: ", signature);
+//   console.log("signature sign: ", sign(req.raw));
+//   // return signature && (signature === sign(req.raw))
+// }
 
-function sign (data) {
-  const buffer = Buffer.from(data, 'utf8');
-  return 'sha1=' + crypto.createHmac('sha1', secret).update(buffer).digest('hex');
-}
+// function sign (data) {
+//   const buffer = Buffer.from(data, 'utf8');
+//   return 'sha1=' + crypto.createHmac('sha1', secret).update(buffer).digest('hex');
+// }
